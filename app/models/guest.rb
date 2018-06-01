e313e4b4d63cd7e0ddb6a25e044746c8b5ccc5db
Anna Royer <anna.royer@colorado.edu>
2018-06-01 10:33:23 -0600

    finish auth

---
+++ app/models/guest.rb
@@ -0,0 +1,5 @@
+class Guest
+  def role
+    return "guest"
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  validates_presence_of :first_name, :last_name, :token, :oauth_expires_at
+  validates :uid, :email, presence: true, uniqueness: true
+  enum role: %w(user admin)
+
+  def self.update_or_create(auth)
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+      uid: auth[:uid],
+      email: auth[:info][:email],
+      first_name: auth[:info][:first_name],
+      last_name: auth[:info][:last_name],
+      token: auth[:credentials][:token],
+      oauth_expires_at: auth[:credentials][:expires_at]
+    }
+    user.save!
+    user
+  end
+end
